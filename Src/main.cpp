084165150e2aaf5ae4118d4188acf34386cb0fee
YutaSeya <bp17069@shibaura-it.ac.jp>
2020-03-13 16:46:11 +0900

    change main

--- Src/main.cpp
+++ Src/main.cpp
@@ -10,43 +10,7 @@
 void led_test();
 void sw_test();
 void mcp3204_test();
-
-void sensor_test()
-{
-  Sensor *sensor = Sensor::getInstance();
-  Sensor_Data sensor_data;
-  int16_t sensor_value[5];
-  std::printf("ctr + c exit this program\n");
-  std::printf("left_front, left, right, right_front, front\n");
-  int count = 0;
-
-  while(1){
-    sensor->update();
-    sensor_data = sensor->get(Sensor::LEFT_FRONT);
-    sensor_value[0] = sensor_data.now;
-
-    sensor_data = sensor->get(Sensor::LEFT);
-    sensor_value[1] = sensor_data.now;
-
-    sensor_data = sensor->get(Sensor::RIGHT);
-    sensor_value[2] = sensor_data.now;
-
-    sensor_data = sensor->get(Sensor::RIGHT_FRONT);
-    sensor_value[3] = sensor_data.now;
-
-    sensor_data = sensor->get(Sensor::FRONT);
-    sensor_value[4] = sensor_data.now;
-
-    std::printf("%4d, %4d, %4d, %4d, %4d\n", sensor_value[0],
-    sensor_value[1],sensor_value[2],sensor_value[3],sensor_value[4] );
-    count++;
-    sleep(1);
-    if(count > 120) break;
-  }
-
-  sensor->turnOffSensorLED();
-
-}
+void sensor_test();
 
 int main()
 {
@@ -99,5 +63,42 @@ void mcp3204_test()
     std::printf("%4d,%4d,%4d,%4d\r\n",ad_data[0], ad_data[1], ad_data[2], ad_data[3]);
     sleep(1);
   }
+
+}
+
+void sensor_test()
+{
+  Sensor *sensor = Sensor::getInstance();
+  Sensor_Data sensor_data;
+  int16_t sensor_value[5];
+  std::printf("ctr + c exit this program\n");
+  std::printf("left_front, left, right, right_front, front\n");
+  int count = 0;
+
+  while(1){
+    sensor->update();
+    sensor_data = sensor->get(Sensor::LEFT_FRONT);
+    sensor_value[0] = sensor_data.now;
+
+    sensor_data = sensor->get(Sensor::LEFT);
+    sensor_value[1] = sensor_data.now;
+
+    sensor_data = sensor->get(Sensor::RIGHT);
+    sensor_value[2] = sensor_data.now;
+
+    sensor_data = sensor->get(Sensor::RIGHT_FRONT);
+    sensor_value[3] = sensor_data.now;
+
+    sensor_data = sensor->get(Sensor::FRONT);
+    sensor_value[4] = sensor_data.now;
+
+    std::printf("%4d, %4d, %4d, %4d, %4d\n", sensor_value[0],
+    sensor_value[1],sensor_value[2],sensor_value[3],sensor_value[4] );
+    count++;
+    sleep(1);
+    if(count > 120) break;
+  }
+
+  sensor->turnOffSensorLED();
 
 }

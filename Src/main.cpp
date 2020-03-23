61023b92adb42d3068fbdc210bcf31b778202bc5
YutaSeya <bp17069@shibaura-it.ac.jp>
2020-03-23 11:52:49 +0900

    check now

--- Src/main.cpp
+++ Src/main.cpp
@@ -126,13 +126,15 @@ void motor_right_test()
   access->clearBit(RPI_GPIO_GPFSEL1, 1 << 9);
   access->setBit(RPI_GPIO_GPFSEL1, 1 << 11);
 
+  // GPIO5 output
+  access->setBit(RPI_GPIO_GPFSEL0, 1 << 15);
   // GPIO6 output 
   access->setBit(RPI_GPIO_GPFSEL0, 1 << 18);
 
   // setting dir bit
   access->setBit(RPI_GPIO_OUTPUT_CLR_0, 1 << 6);
 
-  access->setBit(RPI_GPIO_OUTPUT_CLR_0, 1 << 5);
+  access->setBit(RPI_GPIO_OUTPUT_SET_0, 1 << 5);
 
   access->closePeriperal();
 

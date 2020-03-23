3c53b3d3585501bc868bf12c7d82f99a4684224a
YutaSeya <bp17069@shibaura-it.ac.jp>
2020-03-23 11:49:20 +0900

    check point

--- Src/main.cpp
+++ Src/main.cpp
@@ -132,7 +132,7 @@ void motor_right_test()
   // setting dir bit
   access->setBit(RPI_GPIO_OUTPUT_CLR_0, 1 << 6);
 
-  access->setBit(RPI_GPIO_OUTPUT_SET_0, 1 << 5);
+  access->setBit(RPI_GPIO_OUTPUT_CLR_0, 1 << 5);
 
   access->closePeriperal();
 

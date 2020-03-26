02fecddc5d554b8f4b21b6edb458a43af1282770
YutaSeya <bp17069@shibaura-it.ac.jp>
2020-03-26 10:38:53 +0900

    update main

--- Src/main.cpp
+++ Src/main.cpp
@@ -40,7 +40,7 @@ void motor_test()
   access->setBit(RPI_GPIO_GPFSEL1, 1 << 18);
 
   // GPIO6 output 
-  access->setBit(RPI_GPIO_GPFSEL0, 1 << 18);
+  //access->setBit(RPI_GPIO_GPFSEL0, 1 << 18);
 
   // setting dir bit
   access->setBit(RPI_GPIO_OUTPUT_SET_0, 1 << 5 | 1 << 6);

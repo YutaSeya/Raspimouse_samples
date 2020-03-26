ea91d7844d5bc806a6f79d710ad6dcfc9468c06d
YutaSeya <bp17069@shibaura-it.ac.jp>
2020-03-26 16:55:10 +0900

    update motor

--- Src/motor.cpp
+++ Src/motor.cpp
@@ -101,12 +101,6 @@ void Motor::set(int32_t left, int32_t right)
 
   access->openPeriperal(RPI_GPIO_BASE);
 
-  if(left == 0 && right == 0){
-    access->setBit(RPI_GPIO_OUTPUT_CLR_0, 1 << 5);
-  } else {
-    access->setBit(RPI_GPIO_OUTPUT_SET_0, 1 << 5);
-  }
-
   if(left < 0 ){
     left = -1 * left;
     access->setBit(RPI_GPIO_OUTPUT_SET_0, 1 << 16);
@@ -120,6 +114,12 @@ void Motor::set(int32_t left, int32_t right)
   } else {
     access->setBit(RPI_GPIO_OUTPUT_CLR_0, 1 << 6);
     access->setBit(RPI_GPIO_OUTPUT_SET_0, 1 << 6);
+  }
+
+  if(left == 0 && right == 0){
+    access->setBit(RPI_GPIO_OUTPUT_CLR_0, 1 << 5);
+  } else {
+    access->setBit(RPI_GPIO_OUTPUT_SET_0, 1 << 5);
   }
   
   access->closePeriperal();
